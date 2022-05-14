4a14f40aded6348fe583468678a92597e16cbbc4
t4rcisio <tarcisiobatistaprates@gmail.com>
2022-05-13 23:17:51 -0300

    feat: Basic server config

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 import express from "express";
 
+const app = express();
+const port = 5000;
+
+app.get("/", (request, response) => {
+  response.send("Backend Challenge 2021 🏅 - Covid Daily Cases");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
