be160d466dd3fbde98bdd8d02eea9ecc766eb0a9
t4rcisio <tarcisiobatistaprates@gmail.com>
2022-05-14 19:51:30 -0300

    feat: date route

--- src/route/route.js
+++ src/route/route.js
@@ -1,8 +1,9 @@
 import Router from "express";
 import cases from "./casesRoute.js";
+import date from "./dateRoute.js";
 
 const Routes = Router();
 
 Routes.use("/cases", cases);
-
+Routes.use("/date", date);
 export default Routes;
